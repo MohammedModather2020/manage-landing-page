d195bbbc2a51d635f7522c02e4bbda3a4e8fee06
Mohammed Modather <mohammedmodather144@gmail.com>
2022-09-14 07:57:48 +0200

    Add btn to use click scroll to top page

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 const btnHamburger = document.getElementById('menu-btn');
 const menu = document.getElementById('menu');
+const btnScroll = document.getElementById('scroll-top');
 
 // add click as button hamburger to toggle showing menu as mobile screen size
 btnHamburger.addEventListener('click', () => {
@@ -10,3 +11,20 @@ btnHamburger.addEventListener('click', () => {
 
 // trigger aos animate
 AOS.init();
+
+// change background navbar desktop after page scroll top 20px
+window.onscroll = function () {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    btnScroll.style.display = 'block';
+  } else {
+    btnScroll.style.display = 'none';
+  }
+};
+
+// scroll to top
+btnScroll.addEventListener('click', function () {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
