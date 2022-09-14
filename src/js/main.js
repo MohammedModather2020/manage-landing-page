c3f53c8a99722f0a5f2ff60d7001e44916ea15e8
Mohammed Modather <mohammedmodather144@gmail.com>
2022-09-14 07:17:45 +0200

    Add animation using aos libaray

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,12 @@
 const btnHamburger = document.getElementById('menu-btn');
 const menu = document.getElementById('menu');
 
+// add click as button hamburger to toggle showing menu as mobile screen size
 btnHamburger.addEventListener('click', () => {
   btnHamburger.classList.toggle('open');
   menu.classList.toggle('flex');
   menu.classList.toggle('hidden');
 });
+
+// trigger aos animate
+AOS.init();
